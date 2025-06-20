41afa32ff52bd786635d6cb44b73bd0dabd21b60
Abdur55 <skabdurrahim372@gmail.com>
2025-06-21 05:49:31 +0600

    added all the thing

---
+++ js/Home.js
@@ -0,0 +1,11 @@
+
+// step----1
+document.getElementById('addMoney-btn').addEventListener('click', function(event){
+     event.preventDefault
+    
+    
+     // step---2
+
+ const inputAmount =     document.getElementById('input-amount').value
+ console.log(inputAmount)
+})
--- js/login.js
+++ js/login.js
@@ -1,8 +1,36 @@
+// document.getElementById('login-btn').addEventListener('click', function(event){
+//     // step 2
+//     event.preventDefault();
+    
+//     // step 2 
+//    const phonenumber =  document.getElementById('phone-number').value
+//    const pinnumber =document.getElementById('pin-number').value
+
+//    console.log(phonenumber, pinnumber)
+
+//    if(phonenumber === '120' && pinnumber === '5000'){
+//     console.log(';You have logined')
+//    }
+//    else{
+//     alert('You have some wrong')
+//    }
+// })
+
+
 document.getElementById('login-btn').addEventListener('click', function(event){
-    // step 2
-    event.preventDefault();
-    console.log('this is login btn')
-    // step 2 
-   const phonenumber =  document.getElementById('phone-number').value
-   console.log(phonenumber)
+  event.preventDefault()
+ 
+   
+  const phonenumber = document.getElementById('phone-number').value
+  const pinnumber = document.getElementById('pin-number').value
+  console.log(phonenumber, pinnumber)
+
+  if(phonenumber === '120' && pinnumber === '1000'){
+    console.log('you have logined ')
+    window.location.href = '/Home.html'
+  }
+  else{
+    alert('You have some wrong')
+  }
+
 })
