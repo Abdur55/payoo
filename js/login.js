d9b11f88686e33fd28ce4a9c13286e5337371123
Abdur55 <skabdurrahim372@gmail.com>
2025-06-20 16:36:13 +0600

    this is my frist committ

---
+++ js/login.js
@@ -0,0 +1,8 @@
+document.getElementById('login-btn').addEventListener('click', function(event){
+    // step 2
+    event.preventDefault();
+    console.log('this is login btn')
+    // step 2 
+   const phonenumber =  document.getElementById('phone-number').value
+   console.log(phonenumber)
+})
