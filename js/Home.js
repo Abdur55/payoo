3b759898ffe859b1530b6c099ee58af65093720b
Abdur55 <skabdurrahim372@gmail.com>
2025-06-21 06:43:29 +0600

    add my commit

--- js/Home.js
+++ js/Home.js
@@ -1,11 +1,37 @@
 
 // step----1
 document.getElementById('addMoney-btn').addEventListener('click', function(event){
-     event.preventDefault
-    
-    
-     // step---2
 
- const inputAmount =     document.getElementById('input-amount').value
- console.log(inputAmount)
+    event.preventDefault()
+    
+
+    console.log('this dko adlk')
+    
+    // step---2
+    
+    const inputAmount =  document.getElementById('input-amount').value
+    console.log(inputAmount)
+    const inputpinnumber = document.getElementById('input-pin-number').value
+    console.log(inputpinnumber)
+    
+//  step 3
+
+if(inputpinnumber === '1234'){
+    
+    console.log('You have added Money')
+//     // step ----4
+
+    const currentAmount = document.getElementById('current-Amount').innerText
+    console.log(currentAmount)
+
+  const addMoneynumber = parseFloat(currentAmount)
+  const inputAmountnumber = parseFloat(currentAmount)
+  const newbalence = addMoneynumber + inputAmountnumber
+    console.log(newbalence)
+  document.getElementById('current-Amount').innerText = newbalence
+}
+else{
+    alert('feiled to add money')
+}
+
 })
